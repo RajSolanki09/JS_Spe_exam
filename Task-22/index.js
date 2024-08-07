49bf7380488076aee5177f08f4cff6ca507e603d
3811 Raj Solanki <solankiraj9642@gmail.com>
2024-08-07 10:43:47 +0530

    Task-22

---
+++ Task-22/index.js
@@ -0,0 +1,29 @@
+let users=[]
+
+const uimaker=()=>{
+    document.getElementById('tableBody').innerHTML=""
+    for(let i=0; i<users.length; i++){
+        let tr=document.createElement('tr')
+        let td1=document.createElement('td')
+        td1.innerText=users[i].name
+        let td2=document.createElement('td')
+        td2.innerText=users[i].age
+        let td3=document.createElement('td')
+        td3.innerText=users[i].email
+        tr.append(td1,td2,td3)
+        document.getElementById('tableBody').append(tr)
+        
+    }
+}
+
+const data=(e)=>{
+    e.preventDefault();
+    let user={
+        name:document.getElementById("name").value,
+        age:Number(document.getElementById("age").value),
+        email:document.getElementById("email").value
+    }
+    users.push(user)
+
+}
+document.getElementById("userData").addEventListener("submit", data);
